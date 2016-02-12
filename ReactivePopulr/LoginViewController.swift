9deec33f8a1fcb1a74bf4441cda6b5150db46564
desmond.mcnamee <desmond.mcnamee@teamplace.net>
2016-02-12 18:05:44 +0100

    What the heck am I doing?

---
+++ ReactivePopulr/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ReactivePopulr
+//
+//  Created by Desmond McNamee on 12/02/16.
+//  Copyright © 2016 Populr. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var sublitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let isValidUsername = usernameTextField.rx_text
+            .map { self.isUsername($0)}
+        let isValidPassword = passwordTextField.rx_text
+            .map { self.isPassword($0)}
+        
+        Observable.combineLatest(isValidUsername, isValidPassword) { $0 + " " + $1 }
+        
+    }
+    
+    func isUsername(text: String) -> Bool {
+        return text.characters.count > 5
+    }
+    
+    func isPassword(text: String) -> Bool {
+        return text.characters.count > 5
+    }
+}
+
